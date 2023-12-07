3528f6d8129b6c4eea8a3c89378ffc82996a0ab9
Lucas Dang <dhp371.work@gmail.com>
2023-12-07 23:54:26 +0700

    feat: add cpp image

--- pkg/lighthouse/src/constants/containerImages.constant.ts
+++ pkg/lighthouse/src/constants/containerImages.constant.ts
@@ -2,6 +2,7 @@ const containerImagesConstant: Record<string, any> = {
     baseImage: {
         python: "ghcr.io/phuchptty/yggdrasil/base/python",
         javascript: "ghcr.io/phuchptty/yggdrasil/base/nodejs",
+        cpp: "ghcr.io/phuchptty/yggdrasil/base/cpp",
     },
 };
 
